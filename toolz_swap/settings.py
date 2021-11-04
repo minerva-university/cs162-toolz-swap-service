39eb33810189d23e20e97cd5bf3b348fe1c276ef
Chika-Jinanwa <46027071+Chika-Jinanwa@users.noreply.github.com>
2021-11-04 17:32:27 -0400

    Decouple configs in settings.py to read from .env file

--- toolz_swap/settings.py
+++ toolz_swap/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3xs&f40a!v0sk--iu@mauorr%!igo*rrub84dm-&(*ihip(z=r'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
+print(DEBUG)
+print(type(DEBUG))
 
 ALLOWED_HOSTS = []
 
@@ -103,6 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#Set allowed hosts when DEBUG == False
+ALLOWED_HOSTS = [
+  '127.0.0.1']
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
