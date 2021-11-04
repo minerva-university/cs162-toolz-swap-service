7f87f0ae5808d1868c05ed3a5f526f9d7fd58871
Chika-Jinanwa <46027071+Chika-Jinanwa@users.noreply.github.com>
2021-11-04 17:40:41 -0400

    Decouple configs in settings.py to read from a .env file

--- toolz_swap/settings.py
+++ toolz_swap/settings.py
@@ -16,21 +16,22 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#TODO Decouple Configurations to read from env file
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
-print(DEBUG)
-print(type(DEBUG))
+if os.environ.get("DEBUG") == 'true':
+    DEBUG = True
+else:
+    DEBUG  = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -106,9 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#Set allowed hosts when DEBUG == False
-ALLOWED_HOSTS = [
-  '127.0.0.1']
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
