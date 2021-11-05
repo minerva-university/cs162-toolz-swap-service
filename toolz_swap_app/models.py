23f743b57287f1625e03020c41cc7e3fd3f000dc
h-alekyan <hovhannes.alekyan@minerva.kgi.edu>
2021-11-06 00:28:01 +0900

    added models: user,tool, swaps, listings

--- toolz_swap_app/models.py
+++ toolz_swap_app/models.py
@@ -4,3 +4,28 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+class Tool(models.Model):
+    toolName = models.CharField(max_length=200)
+    toolModel = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.toolName
+
+class Swaps(models.Model):
+    borrowerId =  models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    lenderId =  models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    toolId =  models.ForeignKey(
+        Tool, on_delete=models.CASCADE, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+class Listing(models.Model):
+    lenderId =  models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    toolId =  models.ForeignKey(
+        Tool, on_delete=models.CASCADE, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    expires = models.DateTimeField()
