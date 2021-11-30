29b542c7a84feb6d66309cfbf8ad40dde361457c
h-alekyan <hovhannes.alekyan@minerva.kgi.edu>
2021-11-30 18:17:00 +0900

    added docstrings to the models file

--- toolz_swap_app/models.py
+++ toolz_swap_app/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    """
+    We are using django's abstract user here, which takes care of a basic user table. 
+    """
     pass
 
 class Tool(models.Model):
+    """
+    Tool stores information on individual tools including the name of the tool, the model, 
+    a description and a boolean value of whether it is available or not.
+    
+    """
     toolName = models.CharField(max_length=200)
     toolModel = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
@@ -15,6 +23,11 @@ class Tool(models.Model):
 
 
 class Listing(models.Model):
+    """
+    Listing includes the ID of the tool (foreign key -> Tool) that is being listed, along with the lender's ID (foreign key -> User) 
+    and information on listing creation-exiration dates.
+    
+    """
     lenderId =  models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
     toolId =  models.ForeignKey(
@@ -24,6 +37,16 @@ class Listing(models.Model):
 
 
 class Swaps(models.Model):
+    """
+    Swaps stores information on swaps that have taken place successfully.
+    It contains information on the borrower and lender (2 foreign keys -> User) as well as information on the tool (foreign key -> Tool)
+    and the listing (foreign key -> Listing). 
+    
+    There is also an expiration date, specifying when the tool needs to be returned 
+    (different from expires in listing which indicates the expiration date of the listing).
+
+    """
+
     borrowerId =  models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
     toolId =  models.ForeignKey(
