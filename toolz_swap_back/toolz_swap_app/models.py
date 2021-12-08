b56438d9cea0b7d7ed89f10826670dc6fc776128
Nikita Kotsehub <47567310+nikita-kotsehub@users.noreply.github.com>
2021-12-08 22:28:43 +0100

    added more changes to the models and updated TODO with more tasks

--- toolz_swap_back/toolz_swap_app/models.py
+++ toolz_swap_back/toolz_swap_app/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import AbstractUser
 import uuid
 
 # TODO: Figure out ManyToMany field in User (see comment near saved_places)
+# TODO: Go through the comments and make other suggested changes, such as Phone number in User
 # TODO: make nitty-gritty changes like null=True, blank=True, max_length=number, on_delete=SET_NULL/CASCADE/RESTRICT and others
 # TODO: added choices of 1 to 5 for Rating in ListinsReviews
+# TODO: add representation strings __str__ for the tables
+# TODO: add Admin views in admin.py
 
 '''
 OLD MODELS. SAVED TEMPORARILY FOR IMMEDIATE REFERENCE
@@ -34,43 +37,39 @@ class Swaps(models.Model):
 
 '''
 class Cities(models.Model):
-    city_id=models.UUIDField(primary_key=True)
+    city_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     name=models.CharField()
-    country=models.CharField() # suggest to be UUIDField but I think CharField makes more sense
     population=models.IntegerField()
     size_sqkm=models.FloatField()
 
 class Neighborhoods(models.Model):
-    neighborhood_id=models.UUIDField(primary_key=True)
+    neighborhood_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     name=models.CharField()
     city=models.ForeignKey(Cities, on_delete=models.CASCADE)
     population=models.IntegerField()
     size_sqkm=models.FloatField()
 
 class ToolTypes(models.Model):
-    tool_id=models.UUIDField(primary_key=True)
+    tool_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     name=models.CharField()
     purpose=models.TextField()
     popularity=models.CharField()
 
 class Brands(models.Model):
-    brand_id=models.UUIDField(primary_key=True)
+    brand_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     name=models.CharField()
-    #price_range=models.CharField()
     logo=models.ImageField()
 
-class Models(models.Model):
-    model_id=models.UUIDField(primary_key=True)
+class ToolModels(models.Model):
+    model_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     name=models.CharField()
-    #price_range=models.CharField()
     year_released=models.IntegerField()
     
-
 class User(AbstractUser):
-    username = models.CharField(max_length=40, unique=True)
+    user_id = models.UUIDField(default=uuid.uuid4) 
+    username = models.CharField(max_length=40, unique=True, primary_key=True)
     USERNAME_FIELD = 'username'
 
-    #user_id = models.UUIDField(primary_key=True)
     #first_name=models.CharField() 
     # first_name, last_name, password, and email are default fields in Django User model.
 
@@ -94,21 +93,20 @@ class User(AbstractUser):
 
 class Listings(models.Model):
     # default=uuid.uuid4
-    listing_id = models.UUIDField(primary_key=True, unique=True, help_text='Unique ID for this particular listing')
+    listing_id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, help_text='Unique ID for this particular listing')
     title = models.CharField(max_length=200)
     owner =  models.ForeignKey(
         User, on_delete=models.RESTRICT, null=True)
     brand = models.ForeignKey(Brands, on_delete=models.CASCADE)
-    model = models.ForeignKey(Models, on_delete=models.CASCADE)
+    model = models.ForeignKey(ToolModels, on_delete=models.CASCADE)
     tool_category=models.ForeignKey(ToolTypes, on_delete=models.CASCADE)
     address=models.CharField()
     city=models.ForeignKey(Cities, on_delete=models.CASCADE)
     neighborhood=models.ForeignKey(Neighborhoods, on_delete=models.CASCADE)
     description=models.TextField(max_length=2000)
     created_on=models.DateTimeField(auto_now_add=True)
-
-    # I think it'd make more sense to use Rating (1-5) instead of Likes/Dislikes
-    rating_average = models.FloatField()
+    rating_average = models.FloatField() # average rating of the listing, can be calculated from ListingReviews
+    
     #likes=models.IntegerField()
     #dislikes=models.IntegerField()
 
@@ -116,35 +114,32 @@ class Listings(models.Model):
         return f"<listing_id:{self.listing_id}, title:{self.title};"
 
 class ListingRequest(models.Model):
-    request_id=models.UUIDField(primary_key=True)
+    request_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     listing=models.ForeignKey(Listings, on_delete=models.CASCADE)
     created_on=models.DateTimeField()
     author=models.ForeignKey(User, on_delete=models.CASCADE)
     recipient=models.ForeignKey(User, on_delete=models.CASCADE)
     body=models.TextField()
-
-    # I guess with a CharField we'd have to parse it?
-    # perhaps better to use two DateFields: renting_start and renting_end?
-    #dates_requested=models.CharField() 
     renting_start = models.DateTimeField(auto_now_add=True)
     renting_end = models.DateTimeField()
-
     approved = models.BooleanField()
 
 class ListingReviews(models.Model):
-    review_id=models.UUIDField(primary_key=True)
+    review_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     listing=models.ForeignKey(Listings, on_delete=models.CASCADE)
     created_on=models.DateTimeField(auto_now_add=True)
     author=models.ForeignKey(User, on_delete=models.CASCADE)
     body=models.TextField()
-
     top_review=models.BooleanField()
-    rating = models.IntegerField() # maybe add allowed integers later
-    #likes=models.IntegerField()
-    #dislikes=models.IntegerField()
+    # this is the rating that a borrower can give to the Listing
+    rating = models.IntegerField() # maybe add allowed integers later;
+
+    # these are likes/dislikes for the review, such as if it was helpful
+    review_likes=models.IntegerField()
+    review_dislikes=models.IntegerField()
 
 class ListingImages(models.Model):
-    image_id=models.UUIDField(primary_key=True)
+    image_id=models.UUIDField(primary_key=True, default=uuid.uuid4)
     listing=models.ForeignKey(Listings, on_delete=models.CASCADE)
     created_on=models.DateTimeField(auto_now_add=True)
     author=models.ForeignKey(User, on_delete=models.CASCADE)
