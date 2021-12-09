b52987e58f2ffb5038e87410466ad0099a40f92d
Chika-Jinanwa <46027071+Chika-Jinanwa@users.noreply.github.com>
2021-12-09 12:37:23 -0500

    Removed unnecessary imports

--- server/toolz_swap_app/urls.py
+++ server/toolz_swap_app/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from . import views
-from rest_framework import routers
-from .api import ToolViewSet
-from django.urls import include
 
-# router = routers.DefaultRouter()
-# router.register('tool/', ToolViewSet, 'tool')
+from . import views
 
 urlpatterns = [
     # path('', include(router.urls)),
