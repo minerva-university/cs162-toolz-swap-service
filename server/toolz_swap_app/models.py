d00356b984606028bd166aee0f4f617ed224fbba
h-alekyan <hovhannes.alekyan@minerva.kgi.edu>
2021-12-10 21:53:54 +0900

    clearup

--- server/toolz_swap_app/models.py
+++ server/toolz_swap_app/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.core.validators import RegexValidator
+
 
 
 
