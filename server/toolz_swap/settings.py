2444ce99c6f93a47bc3a84b993966a0b8a4c9ee6
Chika-Jinanwa <46027071+Chika-Jinanwa@users.noreply.github.com>
2021-12-11 13:30:26 -0500

    Updated config to use docker or local db setup configs as set in env variables

--- server/toolz_swap/settings.py
+++ server/toolz_swap/settings.py
@@ -87,19 +87,28 @@ WSGI_APPLICATION = 'toolz_swap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("TOOLZ_APP_DB_DOCKER"),
-        #'NAME': os.environ.get("TOOLZ_APP_DB"),
-        'USER': os.environ.get("POSTGRES_USERNAME"),
-        'PASSWORD': os.environ.get("TOOLZ_APP_DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST_DOCKER"),
-        #'HOST': os.environ.get("DB_HOST_DOCKER"),
-        'PORT': 5432,
+if os.environ.get("IS_LOCAL_SETUP") == "true":  # default config is dockerized, for local setup, set this env variable
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("TOOLZ_APP_DB"),
+            'USER': os.environ.get("POSTGRES_USERNAME"),
+            'PASSWORD': os.environ.get("TOOLZ_APP_DB_PASSWORD"),
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("TOOLZ_APP_DB_DOCKER"),
+            'USER': os.environ.get("POSTGRES_USERNAME"),
+            'PASSWORD': os.environ.get("TOOLZ_APP_DB_PASSWORD"),
+            'HOST': os.environ.get("DB_HOST_DOCKER"),
+            'PORT': 5432,
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
