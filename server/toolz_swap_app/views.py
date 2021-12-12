dfacaebceee3bb1edd220547d02bd31f3827e26c
Chika-Jinanwa <46027071+Chika-Jinanwa@users.noreply.github.com>
2021-12-12 17:13:03 -0500

    Appended backslash to url

--- server/toolz_swap_app/views.py
+++ server/toolz_swap_app/views.py
@@ -11,9 +11,8 @@ from rest_framework.response import Response
 
 from .decorators import custom_login_required
 from .forms import SignUpForm
-from .models import User, Listing, City, Neighborhood
-from .serializers import *
 from .queries import *
+from .serializers import *
 
 JWT_SECRET_KEY = settings.JWT_SECRET_KEY
 INVALIDATED_TOKENS = settings.INVALIDATED_TOKENS
@@ -43,6 +42,7 @@ def login(request):
     :param: request:rest_framework.request.Request
     :return: django-rest-framework Response object
     """
+    print(request.data)
     try:
         username = request.data['username']
         password = request.data['password']
@@ -102,29 +102,27 @@ def get_all_users(request):
     return Response(serializer.data)
 
 
-#@custom_login_required
 @api_view(['GET'])
+@custom_login_required
 def get_all_listings_view(request):
-    #neighborhood_id = request.query_params["neighborhood_id"]
-    #listings = get_all_listings_for_neighborhood(neighborhood_id)
+    # neighborhood_id = request.query_params["neighborhood_id"]
+    # listings = get_all_listings_for_neighborhood(neighborhood_id)
     listings = get_all_listings()
     serializer = ListingSerializer(listings, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-#@custom_login_required
 @api_view(['GET'])
+@custom_login_required
 def get_all_cities_view(request):
     cities = get_all_cities()
     serializer = CitySerializer(cities, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-#@custom_login_required
 @api_view(['GET'])
+@custom_login_required
 def get_all_neighborhoods_view(request):
     neighborhoods = Neighborhood.objects.all()
     serializer = NeighborhoodSerializer(neighborhoods, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
-
-
