f0a33c9f009732a37adcd5945cbf7764f8a5a715
Nahom Agize <nahom@minerva.kgi.edu>
2021-12-17 19:29:22 +0100

    Change class component to functional component
    - Change my listings page from class to functional component because all other pages are functional

--- client/src/pages/MyListings.js
+++ client/src/pages/MyListings.js
@@ -1,94 +1,103 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import { Link } from "react-router-dom";
 import  rating_star from "../images/rating_star.png";
 import "../stylesheets/myListings.css";
 
 
-class MyListings extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          toolList: [],
-          modal: false,
-          allTools: [{
-            tool_id: [1],
-            Title: "hammer for sale",
-            Owner: "",  
-            Brand: "IRWIN", 
-            Model: "UniversalHammer 18V-100",
-            Tool_category: "", 
-            Address: "", 
-            City: "", 
-            Neighborhood: "", 
-            Description: "",
-            toolPrice:66
-          }, {
-            tool_id: [2],
-            Title: "",
-            Owner: "",  
-            Brand: "JBtools", 
-            Model: "Wrench 2001",
-            Tool_category: "", 
-            Address: "", 
-            City: "", 
-            Neighborhood: "", 
-            Description: "",
-            toolPrice:55
-          }, {
-            tool_id: [2],
-            Title: "",
-            Owner: "",  
-            Brand: "JBtools", 
-            Model: "Wrench 2001",
-            Tool_category: "", 
-            Address: "", 
-            City: "", 
-            Neighborhood: "", 
-            Description: "",
-            toolPrice:55
-          }, {
-            tool_id: [2],
-            Title: "",
-            Owner: "",  
-            Brand: "JBtools", 
-            Model: "Wrench 2001",
-            Tool_category: "", 
-            Address: "", 
-            City: "", 
-            Neighborhood: "", 
-            Description: "",
-            toolPrice:55
-          }, {
-            tool_id: [2],
-            Title: "",
-            Owner: "",  
-            Brand: "JBtools", 
-            Model: "Wrench 2001",
-            Tool_category: "", 
-            Address: "", 
-            City: "", 
-            Neighborhood: "", 
-            Description: "",
-            toolPrice:55
-          }, {
-            tool_id: [2],
-            Title: "",
-            Owner: "",  
-            Brand: "JBtools", 
-            Model: "Wrench 2001",
-            Tool_category: "", 
-            Address: "", 
-            City: "", 
-            Neighborhood: "", 
-            Description: "",
-            toolPrice:55
-          }]
-        };
-      }
-    render(){
+const MyListings =()=> {
+
+
+    const [allTools, setallTools]=useState([{
+        "listing_id": "4e8d7df8-e086-4eee-a861-d15505062137",
+        "title": "Some cool Drill right here",
+        "address": "Random Address",
+        "description": "This drill has been used by Mike tyson, you probably want it wink wink",
+        "created_on": "2021-12-14T00:39:28.371000-08:00",
+        "rating_average": 3.2,
+        "owner": "Mike Tyson",
+        "brand": "IRWIN",
+        "model": "X AE-A-12",
+        "tool_category": "DRILL",
+        "city": "San Francisco, CA",
+        "neighborhood": "Tenderloin"
+    }, {
+        tool_id: [1],
+        Title: "hammer for sale",
+        Owner: "",  
+        Brand: "IRWIN", 
+        Model: "UniversalHammer 18V-100",
+        Tool_category: "", 
+        Address: "", 
+        City: "", 
+        Neighborhood: "", 
+        Description: "",
+        toolPrice:66
+      }, {
+        tool_id: [2],
+        Title: "",
+        Owner: "",  
+        Brand: "JBtools", 
+        Model: "Wrench 2001",
+        Tool_category: "", 
+        Address: "", 
+        City: "", 
+        Neighborhood: "", 
+        Description: "",
+        toolPrice:55
+      }, {
+        tool_id: [2],
+        Title: "",
+        Owner: "",  
+        Brand: "JBtools", 
+        Model: "Wrench 2001",
+        Tool_category: "", 
+        Address: "", 
+        City: "", 
+        Neighborhood: "", 
+        Description: "",
+        toolPrice:55
+      }, {
+        tool_id: [2],
+        Title: "",
+        Owner: "",  
+        Brand: "JBtools", 
+        Model: "Wrench 2001",
+        Tool_category: "", 
+        Address: "", 
+        City: "", 
+        Neighborhood: "", 
+        Description: "",
+        toolPrice:55
+      }, {
+        tool_id: [2],
+        Title: "",
+        Owner: "",  
+        Brand: "JBtools", 
+        Model: "Wrench 2001",
+        Tool_category: "", 
+        Address: "", 
+        City: "", 
+        Neighborhood: "", 
+        Description: "",
+        toolPrice:55
+      }, {
+        tool_id: [2],
+        Title: "",
+        Owner: "",  
+        Brand: "JBtools", 
+        Model: "Wrench 2001",
+        Tool_category: "", 
+        Address: "", 
+        City: "", 
+        Neighborhood: "", 
+        Description: "",
+        toolPrice:55
+      }])
+    
+
         return(
             <div className="my-listings-container">
-                  {this.state.allTools.map(tool => (
+                  {allTools.map(tool => (
                   
                   <div className="tool-tile-container">
                     <Link to={`/ListingExpanded`}>
@@ -114,7 +123,7 @@ class MyListings extends Component {
                   ))}
                 </div>
         )
-    }
+    
 }
 
 export default MyListings;
